c58df58a6c23e348fb871e506696a75054770bc4
Olivia Lee <mynameisolivialee@gmail.com>
2026-09-05 11:08:23 +1000

    feat/brand-kit-constansts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+        <h1 className = "bg-brand-red text-white"> Brand Red</h1>
+        <h2 className = "text-brand-blue"> Brand Blue</h2>
+
+
     </main>
   );
 }
