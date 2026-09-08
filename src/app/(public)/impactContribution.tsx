0b60997ec4bc26cd0776f2506f08714acdd32253
Stephanie <seoyoung.min524@gmail.com>
2026-09-09 02:06:18 +1000

    feat: add impact and contribution statements to landing page

---
+++ src/app/(public)/impactContribution.tsx
@@ -0,0 +1,10 @@
+import { ContributionSection, ImpactSection } from "@/components/ui/ui";
+
+export default function Home() {
+    return (
+        <main>
+            <ImpactSection />
+            <ContributionSection />
+        </main>
+    );
+}
