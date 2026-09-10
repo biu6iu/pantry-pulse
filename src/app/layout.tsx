670dd59817166f534fe44be07261b296f83a0341
yehcc <christine.yeh@student.unimelb.edu.au>
2026-09-10 14:10:48 +1000

    basic features of the impact and contribution page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -9,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.variable}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import ImpactContribution from "./(public)/impactContribution";
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+  return <ImpactContribution />;
 }
